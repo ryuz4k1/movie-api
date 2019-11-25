e2118bea88cf078cb0f645b050c6b794c8413ba1
ryuz4k1 <ozerr.murat@hotmail.com>
2019-11-25 13:20:37 +0300

    movies api done

--- routes/movie.js
+++ routes/movie.js
@@ -5,7 +5,59 @@ const Movie = require('../models/movie-model');
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+
+  const movies = Movie.find({});
+  movies.then((data) => {
+    res.send(data);
+  }).catch((err) => {
+    res.send(err);
+  });
+});
+
+router.get('/top10', (req, res, next) => {
+
+  const movies = Movie.find({}).limit(10).sort({ imdbScore: 1});
+  movies.then((data) => {
+    res.send(data);
+  }).catch((err) => {
+    res.send(err);
+  });
+});
+
+router.get('/:movieId', (req, res, next) => {
+  const movie = Movie.findById(req.params.movieId);
+
+  if (!movie) {
+    next({message: 'There no such a film'});
+  }
+
+  movie.then((data) => {  
+    res.send(data);
+  }).catch((err) => {
+    res.send(err);
+  });
+
+});
+
+router.put('/:movieId', (req, res, next) => {
+  const movie = Movie.findOneAndUpdate(
+    req.params.movieId, 
+    req.body,
+    {
+      new: true //Güncellenmiş datayı döndürmek istiyorsak bunu kullanıyoruz.
+    }
+    );
+
+  if (!movie) {
+    next({message: 'There no such a film'});
+  }
+
+  movie.then((data) => {  
+    res.send(data);
+  }).catch((err) => {
+    res.send(err);
+  });
+
 });
 
 router.post('/', (req, res, next) => {
@@ -20,5 +72,39 @@ router.post('/', (req, res, next) => {
 		res.json(err);
 	});
 });
+
+
+router.delete('/:movieId', (req, res, next) => {
+  const movie = Movie.findOneAndDelete(req.params.movieId);
+
+  if (!movie) {
+    next({message: 'There no such a film'});
+  }
+
+  movie.then((data) => {  
+    res.send(data);
+  }).catch((err) => {
+    res.send(err);
+  });
+
+});
+
+
+router.get('/between/:startYear/:endYear', (req, res, next) => {
+  const {startYear, endYear } = req.params;
+
+  const movies = Movie.find({
+    year: { '$gte': parseInt(startYear), '$lte': parseInt(endYear)  }
+  });
+
+  movies.then((data) => {
+    res.send(data);
+  }).catch((err) => {
+    res.send(err);
+  });
+});
+
+
+
 
 module.exports = router;
