7f36bd2f932602c29b4dba19c7de18adbada6483
ryuz4k1 <ozerr.murat@hotmail.com>
2019-11-27 23:21:31 +0300

    deleted server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const App = require("./src/app");
-
-const app = new App().getApp();
-
-app.listen(app.get("PORT"), () => {
-    console.log("Server is running on port: %d", app.get("PORT"));
-});
